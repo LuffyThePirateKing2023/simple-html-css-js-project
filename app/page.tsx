db27a78f1ec104e4909ba4dab36df05a519e530f
JKOfficialrepository <150002044+JKOfficialrepository@users.noreply.github.com>
2024-06-17 19:35:27 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { navItems } from "@/data";
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full scrollbar-hide">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <RecentProjects />
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
---
+++ data/index.ts
@@ -0,0 +1,203 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Simple Age Calculator",
+    des: "Discover your age in an instant with our sleek age calculator. Simply input your birthdate and see your current age appear magically!",
+    img: "/p1.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/age-calculator/",
+  },
+
+   {
+    id: 2,
+    title: "Anime Generator",
+    des: "Explore endless anime characters with our HTML, CSS, and JavaScript-powered generator, fueled by an API. Discover unique styles and personalities with every click!",
+    img: "/p2.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg", "/api.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/anime-generator/",
+  },
+  
+     {
+    id: 3,
+    title: "Basic Calculator",
+    des: "Perform quick calculations with our basic calculator built using HTML, CSS, and JavaScript. Simple, fast, and efficient for everyday math needs",
+    img: "/p3.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/basic-calculator/",
+  },
+
+  {
+    id: 4,
+    title: "BMI Calculator",
+    des: "Calculate your Body Mass Index (BMI) effortlessly with our tool built using HTML, CSS, and JavaScript. Enter your height and weight to instantly see your BMI and health category.",
+    img: "/p4.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/bmi-caclulator/",
+  },
+
+ {
+    id: 5,
+    title: "Real-time Currency Converter",
+    des: "Convert currencies in real-time instantly with our converter built using HTML, CSS, and JavaScript. Enter amounts and select currencies to get real-time exchange rates and conversions",
+    img: "/p5.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg", "/api.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/currency-converter/",
+  },
+
+   {
+    id: 6,
+    title: "Digital Clock",
+    des: "Stay on time with our digital clock built using HTML, CSS, and JavaScript. Displaying the current time with precision, it updates every second for real-time accuracy.",
+    img: "/p6.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/digital-clock/",
+  },
+
+  {
+    id: 7,
+    title: "Englich Dictionary",
+    des: "Enhance your vocabulary with our English dictionary app, built using HTML, CSS, JavaScript, and a dictionary API. Get real-time definitions, synonyms, and usage examples with every search.",
+    img: "/p7.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg", "/api.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/english-dictionary/",
+  },
+
+    {
+    id: 8,
+    title: "Dad-Joke Generator",
+    des: "Brighten your day with our Dad Joke Generator, crafted using HTML, CSS, JavaScript, and a jokes API. Get a new hilarious dad joke with every click!",
+    img: "/p8.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg", "/api.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/joke-generator/",
+  },
+
+  {
+    id: 9,
+    title: "Loan Calculator",
+    des: "Calculate loan payments effortlessly with our loan calculator built using HTML, CSS, and JavaScript. Enter loan details to see instant results on monthly payments and interest.",
+    img: "/p9.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/loan-calculator/",
+  },
+
+  {
+    id: 10,
+    title: "Note Application",
+    des: "Organize your thoughts with our note app, built using HTML, CSS, and JavaScript. Create, edit, and delete notes easily to keep track of your tasks and ideas.",
+    img: "/p10.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/note-app/",
+  },
+
+  {
+    id: 11,
+    title: "Password Generator",
+    des: "Create secure passwords with our regex password generator, built using HTML, CSS, and JavaScript. Customize your criteria and generate strong passwords instantly.",
+    img: "/p11.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg", "/regex.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/password-generator/",
+  },
+
+  {
+    id: 12,
+    title: "Random Color Generator",
+    des: "Explore colors with our random color generator, built using HTML, CSS, and JavaScript. Discover new palettes and hues with every click.",
+    img: "/p12.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/random-color-generator/",
+  },
+
+  {
+    id: 13,
+    title: "Random Quote Generator",
+    des: "Discover inspiration with our random quote generator. Using HTML, CSS, and JavaScript, it delivers insightful quotes with each click.",
+    img: "/p13.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg", "/api.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/random-quote-generator/",
+  },
+
+  {
+    id: 14,
+    title: "Recipe Book Generator",
+    des: "Unlock culinary inspiration with our recipe book generator. Powered by HTML, CSS, and JavaScript, it offers a diverse selection of recipes with every click.",
+    img: "/p14.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg", "/api.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/recipe-book-generator/",
+  },
+
+  {
+    id: 15,
+    title: "Rock Paper Scissor game",
+    des: "Challenge yourself with our rock-paper-scissors game. Enjoy the classic fun of choosing between rock, paper, or scissors, all built with HTML, CSS, and JavaScript.",
+    img: "/p15.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/rock-paper-scissor-game/",
+  },
+  
+  {
+    id: 16,
+    title: "Roll a Dice Game",
+    des: "Roll the dice and test your luck with our interactive dice game. Built using HTML, CSS, and JavaScript, it offers random outcomes and endless fun.",
+    img: "/p16.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/roll-a-dice-app/",
+  },
+
+  {
+    id: 17,
+    title: "Stopwatch Application",
+    des: "Track time accurately with our stopwatch application. Built using HTML, CSS, and JavaScript, it provides precise timing and easy controls for various activities.",
+    img: "/p17.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/stopwatch-app/",
+  },
+
+  {
+    id: 18,
+    title: "Temperature Converter",
+    des: "Convert temperatures seamlessly with our temperature converter. Using HTML, CSS, and JavaScript, it offers quick and accurate conversions between Celsius, Fahrenheit, and Kelvin.",
+    img: "/p18.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/temperature-converter/",
+  },
+
+  {
+    id: 19,
+    title: "Weight Converter",
+    des: "Experience seamless weight conversions with our HTML, CSS, and JavaScript-powered weight converter. Instantly switch between units like pounds, kilograms, and more for quick and accurate results.",
+    img: "/p19.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/weight-converter/",
+  },
+  
+  {
+    id: 20,
+    title: "Testimonial Slider",
+    des: "Explore our interactive testimonial slider, crafted with HTML, CSS, and JavaScript. Easily navigate through customer testimonials for an engaging user experience.",
+    img: "/p20.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    link: "https://luffythepirateking2023.github.io/HTML-CSS-Js-projects/testimonial-slider/",
+  }
+
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
